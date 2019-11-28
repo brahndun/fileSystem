f914bfd8668537f77fe7d9a9ddc6ffe266c96c58
datkinson <datkinson@crimer.com>
2019-11-28 12:23:30 -0600

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -5,6 +5,7 @@
 #define MAX_NAME_SIZE = 128;
 #define NUM_DIRECTLY_MAPPED_INODE_BLOCKS = 9; //How many data blocks each inode maps to
 #define NUM_INDIRECT_BLOCK_MAPPINGS = 206 //512 divided by 2
+#define INODES_PER_INODE_BLOCK = 24
 
 #define INODE_BITMAP_INDEX = 0;
 #define DATA_BITMAP_INDEX = 1;
@@ -14,7 +15,7 @@
                                      //And since we want to support up to 1008 files, we need 1008/24=42 Inode blocks
 #define FIRST_DIRECTORY_ITEM_BLOCK_INDEX = 44; //We want to support up to 1008 files, so we need 1008
                                                 //directory item blocks
-#define LAST_DIR_ENTRY_BLOCK_INDEX = 1051;
+#define LAST_DIRECTORY_ITEM_BLOCK_INDEX = 1051;
 
 #define FIRST_DATA_BLOCK_INDEX = 1052;
 #define LAST_DATA_BLOCK_INDEX = 4999;
@@ -23,13 +24,14 @@ typedef struct FileInternals {
     DirectoryItem directory;
     unsigned short int directoryItemBlockIndex;
     Inode inode;
-    unsigned long int pos = 0;
+    unsigned long int position;
     FileMode fileMode;
 } FileInternals;
 
 typedef struct DirectoryItem {
-    unsigned short int inodeIndex
-    char name[MAX_NAME_SIZE]
+    unsigned short int inodeIndex;
+    char name[MAX_NAME_SIZE];
+    int allocated;
 } DirectoryItem;
 
 typedef struct Inode {
@@ -47,16 +49,164 @@ typedef struct IndirectBlock {
     unsigned short int blocks[NUM_INDIRECT_BLOCK_MAPPINGS]
 } IndirectBlock
 
+typedef struct Bitmap {
+    unsigned char bytes[SOFTWARE_DISK_BLOCK_SIZE];
+}
 
+int setInodeStatus(unsigned short int inodeIndex, int status) {
+    Bitmap bitmap;
+    if (!read_sd_block(&bitmap, INODE_BITMAP_INDEX)) {
+        return 0;
+    }
+    else {
+        if (status)
+            bitmap.bytes[inodeIndex / 8] |= (1 << (7 - (inodeIndex % 8)));
+        else 
+            f.bytes[inodeIndex / 8] &= -(1 << (7 - (blk % 8)));
+        if (!write_sd_block(&bitmap, INODE_BITMAP_INDEX)) {
+            return 0;
+        }
+    }
+}
+
+int setDataBlockStatus(unsigned short int blockIndex, int status) {
+    Bitmap bitmap;
+    if (!read_sd_block(&bitmap, DATA_BITMAP_INDEX)) {
+        return 0;
+    }
+    else {
+        if (status)
+            bitmap.bytes[blockIndex / 8] |= (1 << (7 - (blockIndex % 8)));
+        else 
+            f.bytes[blockIndex / 8] &= -(1 << (7 - (blk % 8)));
+        if (!write_sd_block(&bitmap, DATA_BITMAP_INDEX)) {
+            return 0;
+        }
+    }
+}
+
+int writeInode(unsigned short int inodeIndex, Inode inode) {
+    InodeBlock inodeBlock;
+
+    unsigned short int inodeBlockIndex = inodeIndex / INODES_PER_INODE_BLOCK + FIRST_INODE_BLOCK_INDEX;
+    if (inodeBlockIndex <= LAST_INODE_BLOCK_INDEX) {
+        if (!read_sd_block(&inodeBlock, inodeBlockIndex)) {
+            return 0;
+        }
+        else {
+            ib.inodes[inodeIndex % INODES_PER_INODE_BLOCK] = inode;
+            if (!write_sd_block(&inodeBlock, inodeBlockIndex)) {
+                fserror = FS_IO_ERROR;
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int readInode(unsigned short int inodeIndex, Inode* inode) {
+    InodeBlock inodeBlock;
+    unsigned short int inodeBlockIndex = inodeIndex / INODES_PER_INODE_BLOCK + FIRST_INODE_BLOCK_INDEX;
+    bzero(inode, sizeof(Inode));
+
+    if (inodeBlockIndex > LAST_INODE_BLOCK)
+        return 0;
+    else {
+        if (read_sd_block(&inodeBlock, inodeBlockIndex)) {
+            *inode=inodeBlock.inodes[inodeIndex % INODES_PER_INODE_BLOCK];
+            return 1;
+        }
+            return 0;
+    }
+}
+
+unsigned short int findFreeInodeIndex(void) {
+    Bitmap bitmap;
+
+    if (!read_sd_block(&bitmap, INODE_BITMAP_INDEX)) {
+        fserror=FS_IO_ERROR;
+        return -1;
+    }
+    else {
+        int byte, bit;
+        for (byte = 0; byte < SOFTWARE_DISK_BLOCK_SIZE; byte++) {
+            for (bit = 0; bit < 8; bit++) {
+                if ((bitmap[byte] & (1 << (7 - bit))) == 0) {
+                    return byte*8 + bit;
+                }
+            }
+        }
+    }
+}
+
+unsigned short int findFreeDataBlockIndex(void) {
+    Bitmap bitmap;
+
+    if (!read_sd_block(&f, DATA_BITMAP_INDEX)) {
+        fserror=FS_IO_ERROR;
+        return -1;
+    }
+    else {
+        int byte, bit;
+        for (byte = 0; byte < SOFTWARE_DISK_BLOCK_SIZE; byte++) {
+            for (bit = 0; bit < 8; bit++) {
+                if ((bitmap[byte] & (1 << (7 - bit))) == 0) {
+                    return byte*8 + bit;
+                }
+            }
+        }
+    }
+}
+
+int createDirectoryItem(DirectoryItem directory) {
+    DirectoryItem currentDirectory;
+    for (int i = FIRST_DIRECTORY_ITEM_BLOCK_INDEX; i <= LAST_DIRECTORY_ITEM_BLOCK_INDEX; i++) {
+        if (!read_sd_block(&currentDirectory, i)) {
+            fserror = FS_IO_ERROR;
+            break;
+        }
+        if (currentDirectory.allocated == 0) {
+            writeDirectoryItem(directory, i);
+            return i;
+        }
+
+        
+    }
+
+    return -1;
+
+}
+
+int writeDirectoryItem(DirectoryItem directory, unsigned short int blockIndex) {
+    if (write_sd_block(&directory, blockIndex))
+        return 1;
+    else
+        return 0;
+}
 
 File create_file(char *name, FileMode mode) {
     File f = malloc(sizeof(FileInternals));
     fserror=FS_NONE;
     bzero(f, sizeof(FileInternals));
 
-    //Should probably check if name is null at some point
+    f->mode = mode;
+    f->position=0;
 
-    //directoryBlockIndex = findDirectory()
+    f->d.inode_index = findFreeInodeIndex();
+    f->d.alocated = 1;
+    if (f->d.inode_index < 0)
+        fserror=FS_OUT_OF_SPACE;
+    else {
+        bzero(f->d.inode, sizeof(Inode));
+        if (!writeInode(f->d.inodeIndex, f->inode)) {
+            fserror=FS_IO_ERROR
+        }
+        else {
+            strncpy(f->d.name, name, MAX_NAME_SIZE);
+            f->d.open = 1;
+            createDirectoryEntry(f->directory);
+        }
+    }
 
 
 }
